fa2ca69219c741f49881d834e295625f2c6f1771
Jésus Brakmar <31973542+TERRUSS@users.noreply.github.com>
2018-10-01 14:24:51 +0200

    Update run.js

--- server/public/js/run.js
+++ server/public/js/run.js
@@ -4,7 +4,6 @@ $(function() {
     'ui': $('#emulator').JSNESUI({
       "Installed": [
         ['Super Mario Bros 3', 'roms/Super-Mario-Bros-3-(E).nes'],
-        ['Megaman 2', 'roms/Mega-Man-2.nes'],
       ],
     })
   });
